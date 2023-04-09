0a7d93b20a91a6f572ce580eca13554fc6141c42
Яна <solodovnikovasyv@yandex.ru>
2023-04-09 21:01:26 +0300

    Проинициализирован сервер

---
+++ src/main/java/sc/vsu/ru/server/ServerApplication.java
@@ -0,0 +1,13 @@
+package sc.vsu.ru.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+    }
+
+}
