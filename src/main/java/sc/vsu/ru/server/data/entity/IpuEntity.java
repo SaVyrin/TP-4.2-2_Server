d3fc183eda5d1566846f40c470c21efb9281c805
Яна <solodovnikovasyv@yandex.ru>
2023-04-30 20:45:31 +0300

    Связь сервера и БД в Entity классах (Person, Ipu, Indication)

--- src/main/java/sc/vsu/ru/server/data/entity/IpuEntity.java
+++ src/main/java/sc/vsu/ru/server/data/entity/IpuEntity.java
@@ -1,14 +1,27 @@
 package sc.vsu.ru.server.data.entity;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-import java.util.UUID;
+import java.util.Set;
+
 
 @AllArgsConstructor
+@NoArgsConstructor
+@Entity
 @Getter
+@Table(name = "Ipu")
 public class IpuEntity {
-    final private UUID id;
-    final private UUID personId;
-    final private String type;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name="personal_account", nullable=false)
+    private PersonEntity person;
+    @Column(name = "type", length = 64)
+    private String type;
+    @OneToMany(mappedBy="id", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private Set<IndicationEntity> indications;
 }
--- src/main/java/sc/vsu/ru/server/data/entity/PersonEntity.java
+++ src/main/java/sc/vsu/ru/server/data/entity/PersonEntity.java
@@ -1,17 +1,29 @@
 package sc.vsu.ru.server.data.entity;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-import java.util.UUID;
+import java.util.Set;
 
+
+@Entity
+@NoArgsConstructor
 @AllArgsConstructor
 @Getter
+@Table(name = "Person")
 public class PersonEntity {
-    final private UUID id;
-    final private String login;
-    final private String password;
-    final private String name;
-    final private String lastname;
-    final private String address;
+    @Id
+    private Integer personalAccount;
+    @Column(name = "password", length = 64)
+    private String password;
+    @Column(name = "name", length = 64)
+    private String name;
+    @Column(name = "surname", length = 64)
+    private String surname;
+    @Column(name = "address", length = 128)
+    private String address;
+    @OneToMany(mappedBy="id", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private Set<IpuEntity> ipus;
 }
