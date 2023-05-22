8e8b034a035f45078e3018c36c9fd24ed7cf2cb8
Яна <solodovnikovasyv@yandex.ru>
2023-05-22 03:06:58 +0300

    Добавлена аннотация Swagger в Application класс

--- src/main/java/sc/vsu/ru/server/ServerApplication.java
+++ src/main/java/sc/vsu/ru/server/ServerApplication.java
@@ -1,13 +1,15 @@
 package sc.vsu.ru.server;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableJpaRepositories
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Мои ИПУ", version = "0.0.1", description = "Приложение для передачи показаний ИПУ и оплаты счетов за коммунальные услуги."))
 public class ServerApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ServerApplication.class, args);
     }
