8bf6c5f8e5934d266a87f658bb0f26b98eede421
Яна <solodovnikovasyv@yandex.ru>
2023-05-22 03:07:36 +0300

    Добавлена аннотация Swagger в Controller классы

--- src/main/java/sc/vsu/ru/server/controller/PaymentController.java
+++ src/main/java/sc/vsu/ru/server/controller/PaymentController.java
@@ -1,5 +1,9 @@
 package sc.vsu.ru.server.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,20 +16,28 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/payment")
+@Tag(name = "Платежи")
 public class PaymentController {
     @Autowired
     private PaymentService paymentService;
+
     @GetMapping("/payments")
-    public ResponseEntity<List<PaymentDto>> getPayments(@RequestParam("personalAccount") Integer personalAccount){
+    @Operation(summary = "Получение текущих платежей", description = "Метод возвращает список текущих счетов на оплату для указанного лицевого счета.")
+    @ApiResponse(responseCode = "200", description = "Платежи найдены")
+    public ResponseEntity<List<PaymentDto>> getPayments(@Parameter(name = "Лицевой счет") @RequestParam("personalAccount") Integer personalAccount){
         return new ResponseEntity<>(paymentService.getPayments(personalAccount), HttpStatus.OK);
     }
 
     @GetMapping("/expected")
-    public ResponseEntity<Integer> getExpectedPayment(@RequestParam("personalAccount") Integer personalAccount){
+    @Operation(summary = "Получение ожидаемого платежа", description = "Метод возвращает ожидаемый размер платежа на следующий расчетный период для указанного лицевого счета.")
+    @ApiResponse(responseCode = "200", description = "Ожидаемый платеж расчитан")
+    public ResponseEntity<Integer> getExpectedPayment(@Parameter(name = "Лицевой счет") @RequestParam("personalAccount") Integer personalAccount){
         return new ResponseEntity<>(paymentService.getExpectedPayment(personalAccount), HttpStatus.OK);
     }
 
     @PostMapping("/pay")
+    @Operation(summary = "Оплата счетов", description = "Метод осуществляет оплату всех неоплаченных счетов на данный момент для указанного лицевого счета.")
+    @ApiResponse(responseCode = "200", description = "Счета оплачены")
     public void pay(@RequestBody PersonGetDto personGetDto){
         paymentService.pay(personGetDto.getPersonalAccount());
     }
